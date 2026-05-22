2543c111c309c2adad9c8b275be1105740af3a9b
webpie <w3bpie@gmail.com>
2026-05-22 20:59:38 +0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-FILE_URL="https://raw.githubusercontent.com/webp1e/dkill/main/dkill"
-INSTALL_PATH="/usr/local/bin/dkill"
+url="https://raw.githubusercontent.com/webp1e/dkill/main/dkill"
+insp="/usr/local/bin/dkill"
+insp2="/home/$USER/.local/bin/dkill"
 
 echo "DKILL installer"
 echo "==============="
 
 # скач
 echo -n "скачивание... "
-if curl -fsSL "$FILE_URL" -o "$INSTALL_PATH" 2>/dev/null || \
-   sudo curl -fsSL "$FILE_URL" -o "$INSTALL_PATH" 2>/dev/null; then
+if curl -fsSL "$url" -o "$insp" 2>/dev/null || \
+   sudo curl -fsSL "$url" -o "$insp" 2>/dev/null; then
     echo "OK"
 else
     echo "ошибка скачивания"
@@ -18,7 +19,23 @@ fi
 
 # права
 echo -n "выдача прав... "
-chmod +x "$INSTALL_PATH" 2>/dev/null || sudo chmod +x "$INSTALL_PATH"
+chmod +x "$insp" 2>/dev/null || sudo chmod +x "$insp"
 echo "OK"
 
-echo "готово — запускай: dkill"
+# скач2
+
+echo -n "скачивание... "
+if curl -fsSL "$url" -o "$insp2" 2>/dev/null || \
+   sudo curl -fsSL "$url" -o "$insp2" 2>/dev/null; then
+    echo "OK"
+else
+    echo "ошибка скачивания"
+    exit 1
+fi
+
+# права2
+echo -n "выдача прав... "
+chmod +x "$insp2" 2>/dev/null || sudo chmod +x "$insp2"
+echo "OK"
+
+echo "установка завершена"
